0ed78703065223c7c416ed89491e9da507fa92e4
Nathan Taylor <nathan.taylor.oss@gmail.com>
2016-08-19 19:32:01 -0700

    Project: create tables

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,11 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-
+create table players (name text, id serial primary key);
+create table results (
+    player1 int references players (id),
+    player2 int references players (id),
+    winner int references players (id),
+    primary key (player1, player2),
+    check (player1 < player2 and (winner = player1 or winner = player2))
+);
